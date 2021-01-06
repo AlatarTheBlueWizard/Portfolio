5ead8b7d06a418f50a23965b93831c8d21b29f05
Ryan Gale <gal17027@byui.edu>
2021-01-06 14:54:45 -0500

    fix

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function generateTOC() {
     for (var i = 0; i < links.length; i++) {
         var week = links[i];
         var li = document.createElement('li');
-        li.innerHTML = week;
+        li.innerHTML = week.label;
         tableOfContents.appendChild(li);
     }
 }
