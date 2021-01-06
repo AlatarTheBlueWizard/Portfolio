6bdd0ce091218765fd82399ea93c597cbca7750b
Ryan Gale <gal17027@byui.edu>
2021-01-06 17:51:02 -0500

    small test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 function generateTOC() {
     let tableOfContents = document.getElementById("weeklyWork");
-    const links = [{
-        label: "Week 1",
-        url: "Week1/notesW01.html"
-    }]
+    const links = [
+        {
+            label: "Week 1",
+            url: "Week1/notesW01.html"
+        },
+        {
+            label: "Week 2",
+            url: "Week2/notesW02.html"
+        }
+    ]
 
     for (var i = 0; i < links.length; i++) {
         var week = links[i];
