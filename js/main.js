90e009a625b412e3b198974a9e48973dabfeea26
Ryan Gale <gal17027@byui.edu>
2021-01-06 14:32:54 -0500

    added ol and js list append

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert("Hello, I will be edited later!");
+function generateTOC() {
+    let tableOfContents = document.getElementById("weeklyWork");
+    const links = [{
+        label: "Week 1",
+        url: "../Week1/notesW01.html"
+    }]
+
+    for (var i = 0; i < links.length; i++) {
+        tableOfContents.append('<li>' + links[i] + '</li>');
+    }
+}
