b9e3095d7ada4d94275789fb638d6aebe0f73672
Ryan Gale <gal17027@byui.edu>
2021-01-07 13:56:18 -0500

    adding dynamic list of week1 items

---
+++ Week1/js/W01.js
@@ -0,0 +1,27 @@
+function generateTOC() {
+    let tableOfContents = document.getElementById("notesAndSamples");
+    const links = [
+        {
+            label: "Notes",
+            url: "Week1/Notes/notesW01.html"
+        },
+        {
+            label: "Code Samples",
+            url: "Week1/Code Samples/codeSampleW01.html"
+        }
+    ]
+
+    for (var i = 0; i < links.length; i++) {
+        var week = links[i];
+        var li = document.createElement('li');
+        var text = document.createTextNode(week.label);
+        var a = document.createElement('a');
+
+        a.setAttribute('href', week.url);
+        a.appendChild(text);
+        li.appendChild(a);
+        tableOfContents.appendChild(li);
+    }
+}
+
+generateTOC();
