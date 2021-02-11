59a7c5bde8cb3c32dab941be8e5c93594db685e3
Ryan Gale <gal17027@byui.edu>
2021-02-11 18:47:50 -0500

    todo progress

--- Week6_ToDo_Challenge/js/ToDo.js
+++ Week6_ToDo_Challenge/js/ToDo.js
@@ -1,3 +1,100 @@
 import utils from './utils.js';
 import ls from './ls.js';
 
+// load the list
+loadTodos();
+
+// Add event listener
+document.querySelector('#addBtn').onclick = newTodo;
+document.querySelector('#activeFilter').onclick = applyFilter;
+document.querySelector('#allFilter').onclick = applyFilter;
+document.querySelector('#completedFilter').onclick = applyFilter;
+
+function newTodo() {
+    const todo = createTodo();
+    const todoDiv = createTodoElement(todo);
+    addToList(todoDiv);
+    ls.saveTodo(todo);
+}
+
+function createTodo() {
+    const input = document.querySelector('#todoInput');
+    const newTodo = {id: Date.now(), content: input.value, completed: false}
+    input.value = '';
+    return newTodo;
+}
+
+function createTodoElement(todo) {
+    // Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // complete button
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete-btn');
+
+    // todo content
+    const todoContent = document.createElement('div');
+    todoContent.innerText = todo.content;
+    todoContent.classList.add('todo-content');
+
+    // delete btn
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('data-id', todo.id);
+    deleteBtn.classList.add('todo-delete-btn');
+    deleteBtn.innerText = "X";
+    deleteBtn.onclick = deleteTodo;
+
+    todoDiv.appendChild(completeBtn);
+    todoDiv.appendChild(todoContent);
+    todoDiv.appendChild(deleteBtn);
+
+    return todoDiv;
+}
+
+function addToList(todoDiv) {
+    // Add to the document
+    document.querySelector('#todos').appendChild(todoDiv);
+}
+
+function loadTodos() {
+    const todoList = ls.getTodoList();
+
+    todoList.forEach(element => {
+        const el = createTodoElement(element)
+        addToList(el);
+    })
+}
+
+function deleteTodo(e) {
+    const btn = e.currentTarget;
+    ls.deleteTodo(btn.getAttribute('data-id'));
+    document.querySelector('#todos').innerHTML = '';
+    loadTodos();
+}
+
+function toggleComplete(e) {
+
+}
+
+function applyFilter(e) {
+    // clear the list
+    document.querySelector('#todos').innerHTML = '';
+
+    // declare vars
+    let filteredTodos = [];
+    const allTodos = ls.getTodoList();
+
+    // check which filter to apply
+    if (e.currentTarget.id == 'activeFilter') {
+        filteredTodos = utils.activeFilter(allTodos)
+    } else if (e.currentTarget.id == 'allFilter') {
+        filteredTodos = allTodos
+    }
+
+    // draw the list
+    filteredTodos.forEach(todo => {
+        const el = createTodoElement(todo)
+        addToList(el);
+    })
+}
