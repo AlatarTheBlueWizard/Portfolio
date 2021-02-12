bad7462bb1b49596be839f81bcc1823fa6bd7faa
Ryan Gale <gal17027@byui.edu>
2021-02-12 18:17:50 -0500

    whatever

--- Week6_ToDo_Challenge/js/ToDo.js
+++ Week6_ToDo_Challenge/js/ToDo.js
@@ -33,7 +33,6 @@ function createTodoElement(todo) {
     const completeBtn = document.createElement('button');
     completeBtn.classList.add('complete-btn');
     completeBtn.innerHTML = "&#10003";
-    //completeBtn.onclick = toggleComplete;
 
     // todo content
     const todoContent = document.createElement('div');
