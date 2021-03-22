cab38c2fdf01d95a27db9be0062d161f69995091
Ryan Gale <gal17027@byui.edu>
2021-03-22 16:57:11 -0400

    Created type nav and style edits

--- ChallengeBlockTwo/js/app.js
+++ ChallengeBlockTwo/js/app.js
@@ -2,9 +2,6 @@ const poke = document.getElementById('pokedex');
 const modal = document.getElementById('myModal');
 const span = document.getElementsByClassName('close')[0];
 
-// event listener to display stats
-document.getElementById('pokedex').addEventListener("click", displayStats);
-
 // fetch pokemon data from API
 const getPokemon = () => {
     const promises = [];
@@ -20,11 +17,21 @@ const getPokemon = () => {
             id: data.id,
             name: data.name,
             image: data.sprites['front_default'],
-            // TODO add spec info other than name and img
         }));
         display(pokemon);
     })
 }
+
+/*const getPokemon = () => {
+    for(let i = 1; i <= 50; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+    }
+  }
+  
+  getPokemon(); */
 
 const display = (pokemon) => {
     const pokeString = pokemon.map(eachPokemon => `
@@ -36,9 +43,29 @@ const display = (pokemon) => {
     poke.innerHTML = pokeString;
 }
 
-function displayStats() {
-    modal.style.display = "block";
+const getPokemonStats = () => {
+    const promises = [];
+
+    for (let i = 1; i <= 100; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url)
+            .then(res => res.json()));
+    }
+
+    Promise.all(promises).then(result => {
+        const pokemon = result.map((data) => ({
+            abilities: data.abilities,
+        }));
+        displayStats(pokemon);
+    })
 }
+
+function displayStats() {
+   // TODO display stats in modal
+}
+
+// event listener to display stats
+document.getElementById('pokedex').addEventListener("click", displayStats);
 
 span.onclick = function() {
     modal.style.display = "none";
