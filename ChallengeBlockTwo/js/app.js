42c513f0ae607b4939c7fd31ce6f73fe13dea187
Ryan Gale <gal17027@byui.edu>
2021-03-24 02:50:35 -0400

    added modal functionality

--- ChallengeBlockTwo/js/app.js
+++ ChallengeBlockTwo/js/app.js
@@ -1,4 +1,5 @@
 const poke = document.getElementById('pokedex');
+const pName = document.getElementById('name');
 
 // label styling
 document.getElementById('pTypes').style.color = "yellow";
@@ -7,7 +8,7 @@ document.getElementById('pTypes').style.color = "yellow";
 const getPokemon = () => {
     const promises = [];
 
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 20; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url)
             .then(res => res.json()));
@@ -26,12 +27,17 @@ const getPokemon = () => {
 const display = (pokemon) => {
     // TODO add stats, types, and abilities
     const pokeString = pokemon.map(eachPokemon => `
-    <li id="pId">
+    <li>
         <img src="${eachPokemon.image}" />
         <h3>${eachPokemon.id}. ${eachPokemon.name}</h3>
     </li>
     `).join("");
+    const statString = pokemon.map(eachPokemonTwo => `
+    <h3>${eachPokemonTwo.name}</h3>
+    `).join("");
+    pName.innerHTML = statString;
     poke.innerHTML = pokeString;
+    poke.addEventListener("click", toggleModal);
 }
 
 // types helper
@@ -46,6 +52,34 @@ function createTypes(types, ul) {
 // stats helper
 function stats() {
     // TODO add stats logic
+}
+
+// modal functionality
+var btn = document.getElementById('modal_opener');
+var li = document.getElementById('pId');
+var modal = document.querySelector('.modal');
+
+function attachModalListeners(modalElm) {
+    modalElm.querySelector('.close_modal').addEventListener('click', toggleModal);
+    modalElm.querySelector('.overlay').addEventListener('click', toggleModal);
+}
+
+function detachModalListeners(modalElm) {
+    modalElm.querySelector('.close_modal').removeEventListener('click', toggleModal);
+    modalElm.querySelector('.overlay').removeEventListener('click', toggleModal);
+}
+
+function toggleModal() {
+    var currentState = modal.style.display;
+
+    // If modal is visible, hide it. Else, display it.
+    if (currentState === 'none') {
+        modal.style.display = 'block';
+        attachModalListeners(modal);
+    } else {
+        modal.style.display = 'none';
+        detachModalListeners(modal);
+    }
 }
 
 getPokemon();
